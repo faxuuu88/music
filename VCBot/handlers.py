176816e5f780ef57d8a324b01a5f53f3bb8dd180
faxuuu88 <96454794+faxuuu88@users.noreply.github.com>
2021-12-22 14:12:20 +0000

    Update handlers.py

--- VCBot/handlers.py
+++ VCBot/handlers.py
@@ -75,11 +75,11 @@ async def on_end_handler(client, update: Update):
       print(chat_id)
       op = await skip_current_song(chat_id)
       if op==1:
-         await bot.send_message(chat_id, "`Queue is Empty, Leaving Voice Chat...`")
+         await bot.send_message(chat_id, "`Sin Música, Dejando Chat De Voz...`")
       elif op==2:
-         await bot.send_message(chat_id, "**Some Error Occurred** \n`Clearing the Queues and Leaving the Voice Chat...`")
+         await bot.send_message(chat_id, "**Se Ah Producido Un Error, Salir Del Cht De Voz...`")
       else:
-         await bot.send_message(chat_id, f"**🎧 Now Playing** \n[{op[0]}]({op[1]}) | `{op[2]}`", disable_web_page_preview=True)
+         await bot.send_message(chat_id, f"**🎧 Nueva Música** \n[{op[0]}]({op[1]}) | `{op[2]}`", disable_web_page_preview=True)
    else:
       pass
 
